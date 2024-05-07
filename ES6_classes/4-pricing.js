1d262832c9a426dea38579c084b481e8e515bbbf
Nitsu47 <agustraversa2004@gmail.com>
2024-05-07 14:11:12 -0700

    Task 4 - Pricing class to display and convert prices

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,37 @@
+import Currency from "./3-currency";
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
