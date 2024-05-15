9c833d96ddf72b2478a5866e9b3e8e753d9893e9
Nitsu47 <agustraversa2004@gmail.com>
2024-05-14 17:32:37 -0700

    better error info when catch

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -5,7 +5,7 @@ export default function guardrail(mathFunction) {
     queue.push(value);
   }
   catch (error){
-    queue.push(error.message);
+    queue.push(error.toString());
   }
   queue.push('Guardrail was processed');
   return queue;
