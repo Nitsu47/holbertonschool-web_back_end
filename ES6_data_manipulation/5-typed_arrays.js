56401ba4783a0af772155f678167e2cef3fe2f9b
Nitsu47 <agustraversa2004@gmail.com>
2024-07-05 07:51:58 -0700

    eslint fix

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
-
-  if(position >= length) {
-    throw new Error("Position outside range");
-  }    
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
   const buffArr = new ArrayBuffer(length);
   const editor = new DataView(buffArr);
   editor.setInt8(position, value);
