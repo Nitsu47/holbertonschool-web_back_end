540a03a207962d49ecb8a425c5c1fc6a5ad05a05
Nitsu47 <agustraversa2004@gmail.com>
2024-07-05 07:48:07 -0700

    type error fix

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
 
-  if (position > length) {
+  if(position > length) {
     throw new Error("Position outside range");
   }    
-  const buffArr = new ArrayBuffer(lenght);
+  const buffArr = new ArrayBuffer(length);
   const editor = new(buffArr);
   editor.setInt8(position, value);
 
