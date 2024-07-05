fb61c4eb527de1209c64ebeb6724098575248ccf
Nitsu47 <agustraversa2004@gmail.com>
2024-07-05 16:18:24 -0700

    trailing whitespace

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (startString === '' || typeof startString !== 'string') return '';
-  
+
   const result = [];
   for (const item of set) {
     if (item && item.startsWith(startString)) {
