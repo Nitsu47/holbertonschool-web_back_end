38dd507934ad67b98c47d2306c5caa4a7e87bc6a
Traversak <6758@H135.hbtn.uy>
2024-08-04 00:09:36 -0300

    use of allSettled in task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,24 +2,13 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName)
-    .then((value) => ({
-      status: 'fulfilled',
-      value,
-    }))
-    .catch((err) => ({
-      status: 'rejected',
-      value: err,
-    }));
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
 
-  const photoPromise = uploadPhoto(fileName)
-    .then((value) => ({
-      status: 'fulfilled',
-      value,
+  return Promise.allSettled([userPromise, photoPromise]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
     }))
-    .catch((err) => ({
-      status: 'rejected',
-      value: err,
-    }));
-  return Promise.all([userPromise, photoPromise]);
+  );
 }
