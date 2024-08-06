eac1f5380d996ecec3e60ad55896f4ffec659562
Nitsu47 <agustraversa2004@gmail.com>
2024-08-06 12:09:57 -0300

    Eslint fix

--- ES6_data_manipulation/100-weak.js
+++ ES6_data_manipulation/100-weak.js
@@ -1,14 +1,19 @@
 const weakMap = new WeakMap();
 export default weakMap;
 
-export function queryAPI(endpoint){
-  let count;
-  if (weakMap.has(endpoint))
+export function queryAPI(endpoint) {
+  let count = 0;
+
+  if (weakMap.has(endpoint)) {
     count = weakMap.get(endpoint) + 1;
+  }
   else {
     count = 1;
   }
-  if (count >= 5)
+
+  if (count >= 5) {
     throw new Error('Endpoint load is high');
+  }
+
   weakMap.set(endpoint, count);
 }
